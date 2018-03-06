6fc06cde2e96b28c87db4783f729e640071d3eea
Afonso Mukai <afonso.mukai@esss.se>
2018-03-06 14:09:27 +0100

    Add build_requires to test pkg

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -5,6 +5,7 @@ import os
 class LibrdkafkaTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+    build_requires = "cmake_installer/3.10.0@conan/stable"
 
     def build(self):
         cmake = CMake(self)
